5d02d2a66534fd1eeb4482cb10ed1b0196431d77
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-12-20 21:50:17 +0100

    another refactor fix

--- 01_BLINKLED/build.sh
+++ 01_BLINKLED/build.sh
@@ -16,39 +16,39 @@ if [ $1 = "64" ]
         # use the right compiler toolchain prefix when building on travis
         if [ -z "$2" ]
             then
-                PREFIX=aarch64-elf-
+                _PREFIX=aarch64-elf-
             else
-                PREFIX=aarch64-linux-gnu-
+                _PREFIX=aarch64-linux-gnu-
         fi
-        CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link64.ld"
-        TARGET="aarch64-unknown-linux-gnu"
-        KERNEL="kernel8.img"
+        _CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        _RUSTFLAGS="-C linker=${_PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link64.ld"
+        _TARGET="aarch64-unknown-linux-gnu"
+        _KERNEL="kernel8.img"
 elif [ $1 = "32" ]
     then
         # aarch32
         # use the right compiler toolchain prefix when building on travis
         if [ -z "$2" ]
             then
-                PREFIX=arm-eabi-
+                _PREFIX=arm-eabi-
             else
-                PREFIX=arm-linux-gnueabihf-
+                _PREFIX=arm-linux-gnueabihf-
         fi
-        CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
-        TARGET="armv7-unknown-linux-gnueabihf"
-        KERNEL="kernel7.img"
+        _CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        _RUSTFLAGS="-C linker=${_PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
+        _TARGET="armv7-unknown-linux-gnueabihf"
+        _KERNEL="kernel7.img"
 else
     echo 'provide the archtitecture to be build. Use either "build.sh 32" or "build.sh 64" followed by "deploy" if you like to deploy to the device'
     exit 1
 fi
 
-export CFLAGS="${CFLAGS}"
-export RUSTFLAGS="${RUSTFLAGS}"
-export CC="${PREFIX}cc"
-export AR="${PREFIX}ar"
-export TARGET="${TARGET}"
-export KERNEL="${KERNEL}"
+export CFLAGS="${_CFLAGS}"
+export RUSTFLAGS="${_RUSTFLAGS}"
+export CC="${_PREFIX}cc"
+export AR="${_PREFIX}ar"
+export TARGET="${_TARGET}"
+export KERNEL="${_KERNEL}"
 
-cargo xbuild --target ${TARGET} --release
-cargo objcopy -- -O binary ./target/${TARGET}/release/kernel ./target/${KERNEL}
+cargo xbuild --target ${_TARGET} --release
+cargo objcopy -- -O binary ./target/${_TARGET}/release/kernel ./target/${_KERNEL}
