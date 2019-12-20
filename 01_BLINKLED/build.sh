a239c882a48b7536d0feb69a484ff168c26bdbaf
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-12-20 19:14:14 +0100

    build 01 in aarch32 and 64

--- 01_BLINKLED/build.sh
+++ 01_BLINKLED/build.sh
@@ -4,23 +4,55 @@
 #*********************************************************************************
 set +ev
 
-# aarch64
-export CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-export RUSTFLAGS="-C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link64.ld"
-# if there is any 1 command line argument given don't set the CC/AR variables
-# this is required to be set only when building locally, but not on travis
-# so we pass ./build.sh travis to build without those variables set
-if [ -z "$1" ]
-    then
-        export CC="aarch64-elf-gcc"
-        export AR="aarch64-elf-ar"
-        export RUSTFLAGS="-C linker=aarch64-elf-gcc ${RUSTFLAGS}"
-    else
-        CARGOFLAGS="--verbose" 
+if [ $# -eq 0 ] 
+    then 
+        echo "not enough parameter given"
+        exit 1
 fi
 
-cargo xbuild --target aarch64-unknown-linux-gnu --release ${CARGOFLAGS}
-if [ -z "$1" ]
+# chekc which aarch version to build
+if [ $1 = "64" ]
     then
-        cargo objcopy -- -O binary ./target/aarch64-unknown-linux-gnu/release/kernel ./target/kernel8.img
+        # aarch64
+        export CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        export RUSTFLAGS="-C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C link-arg=-T./link64.ld -C opt-level=3 -C debuginfo=0"
+        if [ -z "$3" ]
+            then
+                export CC="aarch64-elf-gcc"
+                export AR="aarch64-elf-ar"
+                export RUSTFLAGS="-C linker=aarch64-elf-gcc ${RUSTFLAGS}"
+        fi
+        cargo xbuild --target aarch64-unknown-linux-gnu --release --all
+        if [ -z "$3" ]
+            then
+                cargo objcopy -- -O binary ./target/aarch64-unknown-linux-gnu/release/kernel ./target/kernel8.img
+                # after build deploy to device using serial port
+                if [ "$2" = "deploy" ]
+                    then
+                        cargo ruspiro-push -k ./target/kernel8.img -p COM5
+                fi
+        fi
+elif [ $1 = "32" ]
+    then
+        # aarch32
+        export CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        export RUSTFLAGS="-C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
+        if [ -z "$3" ]
+            then
+                export CC="arm-eabi-gcc"
+                export AR="arm-eabi-ar"
+                export RUSTFLAGS="-C linker=arm-eabi-gcc ${RUSTFLAGS}"
+        fi
+        cargo xbuild --target armv7-unknown-linux-gnueabihf --release --bin kernel --target-dir ./target/
+        if [ -z "$3" ]
+            then
+                cargo objcopy -- -O binary ./target/armv7-unknown-linux-gnueabihf/release/kernel ./target/kernel7.img
+                # after build deploy to device using serial port
+                if [ "$2" = "deploy" ]
+                    then
+                        cargo ruspiro-push -k ./target/kernel7.img -p COM5
+                fi
+        fi
+else
+    echo 'provide the archtitecture to be build. Use either "build.sh 32" or "build.sh 64" followed by "deploy" if you like to deploy to the device'
 fi
