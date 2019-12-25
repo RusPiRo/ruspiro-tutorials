0221246ee3597265ad6273df8c7ab404e2e36233
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-12-25 21:13:47 +0100

    fix build scripts for travis

--- 02_CONSOLE/build.sh
+++ 02_CONSOLE/build.sh
@@ -16,39 +16,39 @@ if [ $1 = "64" ]
         # use the right compiler toolchain prefix when building on travis
         if [ -z "$2" ]
             then
-                _PREFIX=aarch64-elf-
+                PREFIX=aarch64-elf-
             else
-                _PREFIX=aarch64-linux-gnu-
+                PREFIX=aarch64-linux-gnu-
         fi
-        _CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        _RUSTFLAGS="-C linker=${_PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link64.ld"
-        _TARGET="aarch64-unknown-linux-gnu"
-        _KERNEL="kernel8.img"
+        CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link64.ld"
+        TARGET="aarch64-unknown-linux-gnu"
+        KERNEL="kernel8.img"
 elif [ $1 = "32" ]
     then
         # aarch32
         # use the right compiler toolchain prefix when building on travis
         if [ -z "$2" ]
             then
-                _PREFIX=arm-eabi-
+                PREFIX=arm-eabi-
             else
-                _PREFIX=arm-linux-gnueabihf-
+                PREFIX=arm-linux-gnueabihf-
         fi
-        _CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        _RUSTFLAGS="-C linker=${_PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
-        _TARGET="armv7-unknown-linux-gnueabihf"
-        _KERNEL="kernel7.img"
+        CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
+        TARGET="armv7-unknown-linux-gnueabihf"
+        KERNEL="kernel7.img"
 else
     echo 'provide the archtitecture to be build. Use either "build.sh 32" or "build.sh 64" followed by "deploy" if you like to deploy to the device'
     exit 1
 fi
 
-export CFLAGS="${_CFLAGS}"
-export RUSTFLAGS="${_RUSTFLAGS}"
-export CC="${_PREFIX}gcc"
-export AR="${_PREFIX}ar"
-export TARGET="${_TARGET}"
-export KERNEL="${_KERNEL}"
+export CFLAGS="${CFLAGS}"
+export RUSTFLAGS="${RUSTFLAGS}"
+export CC="${PREFIX}gcc"
+export AR="${PREFIX}ar"
+export TARGET="${TARGET}"
+export KERNEL="${KERNEL}"
 
-cargo xbuild --target ${_TARGET} --release
-cargo objcopy -- -O binary ./target/${_TARGET}/release/kernel ./target/${_KERNEL}
+cargo xbuild --target ${TARGET} --release
+cargo objcopy -- -O binary ./target/${TARGET}/release/kernel ./target/${KERNEL}
--- 04_I2C/build.sh
+++ 04_I2C/build.sh
@@ -13,31 +13,42 @@ fi
 if [ $1 = "64" ]
     then
         # aarch64
-        _CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        _RUSTFLAGS="-C linker=aarch64-elf-gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link64.ld"
-        _CC="aarch64-elf-gcc"
-        _AR="aarch64-elf-ar"
-        _TARGET="aarch64-unknown-linux-gnu"
-        _KERNEL="kernel8.img"
+        # use the right compiler toolchain prefix when building on travis
+        if [ -z "$2" ]
+            then
+                PREFIX=aarch64-elf-
+            else
+                PREFIX=aarch64-linux-gnu-
+        fi
+        CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link64.ld"
+        TARGET="aarch64-unknown-linux-gnu"
+        KERNEL="kernel8.img"
 elif [ $1 = "32" ]
     then
-        _CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        _RUSTFLAGS="-C linker=arm-eabi-gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
-        _CC="arm-eabi-gcc"
-        _AR="arm-eabi-ar"
-        _TARGET="armv7-unknown-linux-gnueabihf"
-        _KERNEL="kernel7.img"
+        # aarch32
+        # use the right compiler toolchain prefix when building on travis
+        if [ -z "$2" ]
+            then
+                PREFIX=arm-eabi-
+            else
+                PREFIX=arm-linux-gnueabihf-
+        fi
+        CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
+        TARGET="armv7-unknown-linux-gnueabihf"
+        KERNEL="kernel7.img"
 else
-    echo 'provide the archtitecture to be build. Use either "build.sh 32" or "build.sh 64".'
+    echo 'provide the archtitecture to be build. Use either "build.sh 32" or "build.sh 64" followed by "deploy" if you like to deploy to the device'
     exit 1
 fi
 
-export CFLAGS="${_CFLAGS}"
-export RUSTFLAGS="${_RUSTFLAGS}"
-export CC="${_CC}"
-export AR="${_AR}"
-export TARGET="${_TARGET}"
-export KERNEL="${_KERNEL}"
+export CFLAGS="${CFLAGS}"
+export RUSTFLAGS="${RUSTFLAGS}"
+export CC="${PREFIX}gcc"
+export AR="${PREFIX}ar"
+export TARGET="${TARGET}"
+export KERNEL="${KERNEL}"
 
-cargo xbuild --target ${_TARGET} --release
-cargo objcopy -- -O binary ./target/${_TARGET}/release/kernel ./target/${_KERNEL}
+cargo xbuild --target ${TARGET} --release
+cargo objcopy -- -O binary ./target/${TARGET}/release/kernel ./target/${KERNEL}
