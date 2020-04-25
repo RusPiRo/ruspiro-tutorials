5292cf6e899b38eccb988ca687ed6638263d89a7
2ndTaleStudio <43264484+2ndTaleStudio@users.noreply.github.com>
2020-04-25 16:18:35 +0200

    initial commit

--- 02_CONSOLE/build.sh
+++ 02_CONSOLE/build.sh
@@ -16,13 +16,13 @@ if [ $1 = "64" ]
         # use the right compiler toolchain prefix when building on travis
         if [ -z "$2" ]
             then
-                PREFIX=aarch64-elf-
+                PREFIX=aarch64-none-elf-
             else
                 PREFIX=aarch64-linux-gnu-
         fi
         CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link64.ld"
-        TARGET="aarch64-unknown-linux-gnu"
+        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C link-arg=-nostartfiles -C link-arg=-T./link64.ld"
+        TARGET="aarch64-unknown-none"
         KERNEL="kernel8.img"
 elif [ $1 = "32" ]
     then
@@ -30,16 +30,16 @@ elif [ $1 = "32" ]
         # use the right compiler toolchain prefix when building on travis
         if [ -z "$2" ]
             then
-                PREFIX=arm-eabi-
+                PREFIX=arm-none-eabi-
             else
                 PREFIX=arm-linux-gnueabihf-
         fi
-        CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
-        TARGET="armv7-unknown-linux-gnueabihf"
+        CFLAGS="-mcpu=cortex-a53 -march=armv7-a -mfpu=neon -mfloat-abi=softfp -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        RUSTFLAGS="-C linker=${PREFIX}gcc -C target-cpu=cortex-a53 -C link-arg=-nostartfiles -C link-arg=-T./link32.ld"
+        TARGET="armv7a-none-eabi"
         KERNEL="kernel7.img"
 else
-    echo 'provide the archtitecture to be build. Use either "build.sh 32" or "build.sh 64" followed by "deploy" if you like to deploy to the device'
+    echo 'provide the archtitecture to be build. Use either "build.sh 32" or "build.sh 64"'
     exit 1
 fi
 
@@ -47,8 +47,9 @@ export CFLAGS="${CFLAGS}"
 export RUSTFLAGS="${RUSTFLAGS}"
 export CC="${PREFIX}gcc"
 export AR="${PREFIX}ar"
-export TARGET="${TARGET}"
-export KERNEL="${KERNEL}"
 
-cargo xbuild --target ${TARGET} --release
-cargo objcopy -- -O binary ./target/${TARGET}/release/kernel ./target/${KERNEL}
+cargo xbuild --target ${TARGET} --release && ${PREFIX}objcopy -O binary ./target/${TARGET}/release/kernel ./target/${KERNEL}
+# cargo objcopy tries to re-build the crates without the propper cross compile target since whatever version
+# so don't use it but rather use the arm toolchain objcopy to achieve the same
+#cargo objcopy -- -O binary ./target/${TARGET}/release/kernel ./target/${KERNEL}
+
